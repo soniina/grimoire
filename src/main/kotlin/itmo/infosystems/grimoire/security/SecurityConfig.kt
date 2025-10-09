ee034df6fa43882e31148d107d2d4b8c16ec913e
soniina <sonikpav@gmail.com>
2025-10-09 23:07:11 +0300

    feat: add JWTAuth filter and security config

---
+++ src/main/kotlin/itmo/infosystems/grimoire/security/SecurityConfig.kt
@@ -0,0 +1,31 @@
+package itmo.infosystems.grimoire.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+@Configuration
+class SecurityConfig(private val jwtAuthenticationFilter: JwtAuthenticationFilter) {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf { it.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .authorizeHttpRequests { auth ->
+                auth
+                    .requestMatchers("/auth/**").permitAll()
+                    .anyRequest().authenticated()
+            }
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+
+        return http.build()
+    }
+
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
+}
---
+++ src/main/kotlin/itmo/infosystems/grimoire/security/WizardPrincipal.kt
@@ -0,0 +1,6 @@
+package itmo.infosystems.grimoire.security
+
+data class WizardPrincipal (
+    val wizardId: Long,
+    val login: String
+)
