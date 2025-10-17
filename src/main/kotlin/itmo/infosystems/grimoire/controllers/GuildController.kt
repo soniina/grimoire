8c509c1bb646d1da9aff4c110d6341a602c27d80
soniina <sonikpav@gmail.com>
2025-10-17 20:50:10 +0300

    feat: implement endpoint for returning available guilds

---
+++ src/main/kotlin/itmo/infosystems/grimoire/controllers/GuildController.kt
@@ -0,0 +1,20 @@
+package itmo.infosystems.grimoire.controllers
+
+import itmo.infosystems.grimoire.services.GuildService
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import itmo.infosystems.grimoire.security.WizardPrincipal
+import itmo.infosystems.grimoire.models.Guild
+
+
+@RestController
+@RequestMapping("/guilds")
+class GuildController(private val guildService: GuildService) {
+
+    @GetMapping("/available")
+    fun getAvailableGuilds(@AuthenticationPrincipal principal: WizardPrincipal): List<Guild> {
+        return guildService.getAvailableGuilds(principal.id)
+    }
+}
